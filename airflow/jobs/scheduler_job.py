17f8ea09323b231b53e0a5a53dcdef93e2eec4e2
Anthony Panat <anthonypanat@Anthonys-MacBook-Pro-2.local>
2022-09-08 17:38:40 -0400

    add back empty line

--- airflow/jobs/scheduler_job.py
+++ airflow/jobs/scheduler_job.py
@@ -116,6 +116,7 @@ class SchedulerJob(BaseJob):
         **kwargs,
     ):
         self.subdir = subdir
+
         self.num_runs = num_runs
         # In specific tests, we want to stop the parse loop after the _files_ have been parsed a certain
         # number of times. This is only to support testing, and isn't something a user is likely to want to
